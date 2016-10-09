65cf488fbffca5c572c444885e5d2d9a555d7d4e
AlwaysTPW <mingyuanwang99@gmail.com>
2016-10-09 01:22:13 -0700

    Database Connection Manager

--- src/com/objectDef/DBConnManager.java
+++ src/com/objectDef/DBConnManager.java
@@ -1,5 +1,29 @@
 package com.objectDef;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnManager {
 
+	public static Connection getMySQLConnection() throws ClassNotFoundException, SQLException {
+		
+		String hostName = "localhost";
+		String dbName = "searchWord";
+		String userName = "root";
+		String password = "yuriko";
+		
+		return getMySQLConnection(hostName, dbName, userName, password);
+	}
+	
+	public static Connection getMySQLConnection(String hostName, String dbName, String userName, String password) throws SQLException, ClassNotFoundException {
+		// Declare the class Driver for MySQL DB
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// URL Connection for MySQL
+		// Example: jdbc:mysql://localhost:3306/searchWord
+		String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+		Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+		return conn;
+	}
 }
